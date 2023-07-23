49b23d7f5cd45caa35934cc2721c843194baf56d
Peng <17637473650@163.com>
2023-07-23 20:32:14 +0800

    test: 测试云服务更新接口

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('欢迎使用微信云托管！')
 })
 
+app.get('/test', (req, res) => {
+  res.send('哈哈哈哈哈 你好')
+})
+
 const port = process.env.PORT || 80
 app.listen(port, () => {
   console.log('服务启动成功，端口：', port)
